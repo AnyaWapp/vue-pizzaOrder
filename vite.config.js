02afebf714efaabfd7f2c0285bb0ed13086a9e8a
Anya Wappenstein <awwappenstein@gmail.com>
2025-03-27 16:06:33 -0400

    delete slash

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://anyawapp.github.io/vue-pizzaOrder/",
+  base: "https://anyawapp.github.io/vue-pizzaOrder",
   plugins: [
     vue(),
     vueDevTools(),
