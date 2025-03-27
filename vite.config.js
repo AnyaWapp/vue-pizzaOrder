0db75227583fd5ce6b6e3e82a0d835f178d94eef
Anya Wappenstein <awwappenstein@gmail.com>
2025-03-27 15:59:50 -0400

    base change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/ThreePizzaPages/",
+  base: "https://anyawapp.github.io/vue-pizzaOrder/",
   plugins: [
     vue(),
     vueDevTools(),
